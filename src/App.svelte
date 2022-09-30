583b6df98e6099ef091bcc626a7cce76ea56fee6
nursyahjaya <nursyahjaya@gmail.com>
2022-09-30 13:04:45 +0700

    update

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+
+</script>
+
+<main class="flex justify-center h-screen items-center">
+  <div class="flex-col text-center">
+    <h1>Vite + Svelte</h1>
+    <Counter />
+  </div>  
+</main>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,12 @@
+<script>
+  let count = 0
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button on:click={increment}>
+  count is <span class="border px-3 py-0 bg-gray-500 text-white">
+    {count}
+  </span> 
+</button>
